c4d42f105e17b7875e93de59b551f99794630c5e
andis.bizuns <andis.bizuns@edi.lv>
2024-06-21 13:31:05 +0300

    Minor bug fix for element closing tag in demo(...)

--- src/goit/classes/CheckVHDL.py
+++ src/goit/classes/CheckVHDL.py
@@ -225,20 +225,21 @@ class CheckVHDL(Check):
                 fmt = FormatElement(element)
                 demo.append("{}{c0}{:{w}}{c1}{}".format(fmt.tabs, element.name, element.note, w=fmt.width, c0=fmt.c0, c1=fmt.c1))
 
-                # A smaller depth for the next element means that a record must be created with the end of the element
-                if next_element.depth < element.depth:
-                    current_depth = element.depth
-                    parent        = elements[element.parent_id]
-                    
-                    # Closes all elements until the level is the same as the next element
-                    while next_element.depth < current_depth: 
+            # A smaller depth for the next element means that a record must be created with the end of the element
+            if next_element.depth < element.depth:
+                current_depth = element.depth
+                parent        = elements[element.parent_id]
+                
+                # Closes all elements until the level is the same as the next element
+                while next_element.depth < current_depth:
+                    if parent.type == "cod": 
                         fmt = FormatElement(parent)
                         demo.append("{}{c0}{:{w}}{c1}".format(fmt.tabs, parent.end, w=fmt.width, c0=fmt.c0, c1=fmt.c1))
 
-                        if parent.parent_id != 0:
-                            parent = elements[parent.parent_id]
+                    if parent.parent_id != 0:
+                        parent = elements[parent.parent_id]
 
-                        current_depth -= 1
+                    current_depth -= 1
 
         return demo
 
